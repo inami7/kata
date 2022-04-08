61283d2bfbc1675d7744abb144e99691326b87eb
inami7 <103008905+inami7@users.noreply.github.com>
2022-04-08 15:24:04 +0900

    Initial commit

---
+++ src/Todo.tsx
@@ -0,0 +1,12 @@
+//completed?  ?をつけることで別に渡さなくてもいいデータとして扱える
+type TodoType = {
+  userId: number;
+  title: string;
+  completed?: boolean;
+};
+
+export const Todo = (props: TodoType) => {
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return <p>{`${completeMark}${title}(ユーザー:${userId})`}</p>;
+};
